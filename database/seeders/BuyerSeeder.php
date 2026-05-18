1fc93a6ba2f70d9f7f6243656944dd78b335a1dd
Abinaya Arya Zaidan <agus8u7@gmail.com>
2026-05-18 14:33:21 +0700

    feat: add BuyerSeeder to DatabaseSeeder for improved data seeding

---
+++ database/seeders/BuyerSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Profile;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class BuyerSeeder extends Seeder
+{
+    /**
+     * Akun demo pembeli + profil lengkap (bisa langsung ke menu).
+     */
+    public function run(): void
+    {
+        $user = User::updateOrCreate(
+            ['email' => 'pembeli@segosambelan.com'],
+            [
+                'name' => 'Pembeli Demo',
+                'password' => Hash::make('password'),
+                'role' => 'buyer',
+                'email_verified_at' => now(),
+            ]
+        );
+
+        Profile::updateOrCreate(
+            ['user_id' => $user->id],
+            [
+                'phone' => '081234567890',
+                'street_address' => 'Jl. Sambal Nusantara No. 88',
+                'city' => 'Surabaya',
+                'province' => 'Jawa Timur',
+                'postal_code' => '60241',
+            ]
+        );
+    }
+}
